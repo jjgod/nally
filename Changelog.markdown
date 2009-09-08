525ab022b8818ea82343de9fe80c47435f994759
gzjjgod <gzjjgod@db928f3c-c32d-11dd-ba5c-d3b7c2b3b534>
2009-09-08 05:26:45 +0000

    Fix some packaging issues.
    git-svn-id: https://nally.googlecode.com/svn/trunk@310 db928f3c-c32d-11dd-ba5c-d3b7c2b3b534

---
+++ Changelog.markdown
@@ -0,0 +1,17 @@
+## Nally 1.4.5
+2009/09/07
+
+* Fix file name issue on image preview. (jjgod)
+
+## Nally 1.4.4
+2009/09/04
+
+* Fix ssh connection issue. (jjgod)
+* Fix icon disappear issue on Tab items. (jjgod)
+* Fix attachment file name decoding in newsmth. (jjgod)
+* Launch as 32-bit by default in Leopard. (jjgod)
+
+## Nally 1.4.3 (2009/08/28)
+
+* Support 64-bit and Snow Leopard. (jjgod)
+
--- package.py
+++ package.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Package a bundle and generate Sparkle appcasts for release
 
-import sys, os, commands, time, plistlib
+import sys, os, commands, time, plistlib, markdown2
 
 try:
     from jinja2 import Environment, FileSystemLoader
@@ -25,10 +25,8 @@ def package_bundle(bundle_path, zip_parent):
 
     plist           = plistlib.readPlist("%s/Contents/Info.plist" % bundle_path)
     bundle_name     = plist["CFBundleName"]
-    appcast         = "%s.xml" % bundle_name
     appcast_url     = plist["SUFeedURL"]
     bundle_version  = plist["CFBundleVersion"]
-    relnotes_url    = "%s/Release.html" % parent_url(appcast_url)
     zip             = "%s-%s.zip" % (bundle_name, bundle_version)
     zip_url         = "%s/%s" % (zip_parent, zip)
     priv_key        = os.path.expanduser("~/.ssh/%s.private.pem" % bundle_name.lower())
@@ -38,7 +36,7 @@ def package_bundle(bundle_path, zip_parent):
 
     cwd = os.getcwd();
     os.chdir(os.path.dirname(bundle_path))
-    os.system("zip -qr %s/%s %s" % (cwd, zip, os.path.basename(bundle_path)))
+    os.system("zip -qry %s/%s %s" % (cwd, zip, os.path.basename(bundle_path)))
     os.chdir(cwd)
 
     print "[PACK] Signing %s..." % zip
@@ -46,28 +44,31 @@ def package_bundle(bundle_path, zip_parent):
                                 'openssl dgst -dss1 -sign "%s" | '\
                                 'openssl enc -base64' % (zip, priv_key))
 
-    print "[PACK] Generating %s..." % appcast
     env = Environment(loader=FileSystemLoader(sys.path[0]))
     template = env.get_template("appcast.template.xml")
 
-    output = open(appcast, "w")
+    for lang in [ "en", "zh_CN", "zh_TW" ]:
+        if lang == "en":
+            suffix = ""
+        else:
+            suffix = ".%s" % lang
+        relnotes = markdown2.markdown_path("Changelog%s.markdown" % suffix)
+        appcast  = "%s%s.xml" % (bundle_name, suffix)
+        print "[PACK] Generating %s..." % appcast
+        output   = open(appcast, "w")
 
-    output.write(template.render(appName = bundle_name,
-                                 link    = appcast_url,
-                                 relNotes= relnotes_url,
-                                 url     = zip_url,
-                                 date    = date,
-                                 version = bundle_version,
-                                 length  = os.path.getsize(zip),
-                                 signed  = signed).encode("utf-8"))
+        output.write(template.render(appName = bundle_name,
+                                     link    = appcast_url,
+                                     relNotes= relnotes,
+                                     url     = zip_url,
+                                     date    = date,
+                                     version = bundle_version,
+                                     length  = os.path.getsize(zip),
+                                     signed  = signed).encode("utf-8"))
 
-    output.close()
+        output.close()
 
-    print """Done! Please:
-    1. Publish %s to %s
-    2. Publish %s to %s
-    3. Update release notes at %s.""" % (appcast, appcast_url,
-                                         zip, zip_url, relnotes_url)
+    print "Done! Please publish %s to %s." % (zip, zip_url)
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
